90b8592edfcabd5a7be657c362fdb5a2698441aa
Michael Hatton <mhatton@mavericks-vfx.com>
2021-01-20 09:19:13 -0500

    lan - 'update before_app_launch'

--- hooks/tk-multi-launchapp/before_app_launch.py
+++ hooks/tk-multi-launchapp/before_app_launch.py
@@ -50,8 +50,7 @@ class BeforeAppLaunch(tank.Hook):
         # os.environ["MY_SETTING"] = "foo bar"
 
         if engine_name == "tk-maya":
-            self.logger.debug('[PIPELINE] Launching Maya')
-            self.logger.debug('[PIPELINE] Maya version: %s' % version)
+            self.logger.debug('[PIPELINE] Launching Maya version: %s' % version)
 
             # disable Maya reporting
             os.environ["MAYA_DISABLE_CIP"] = "1"
@@ -64,22 +63,18 @@ class BeforeAppLaunch(tank.Hook):
                                                + version + "/plug-ins/bifrost:/usr/autodesk/maya" + version + "/plug-ins/bifrost/plug-ins" )
             os.environ["MAYA_PLUG_IN_PATH"]=os.environ["SHARED_MAYA_DIR"] + "/plugins"
             os.environ["MAYA_RENDER_ENGINE"] = "/array/X/Library/renderer"
-
-            # set RV as default application for sequence playback.
-            
-            #Show menu
-            # import sgtk
-            # import maya.mel as mel
-            # maya_main_window = mel.eval('$temp1=$gMainWindow')
-            # context = sgtk.platform.current_engine().context; 
-            # sgProject = context.project[ 'name' ]
-            # if cmds.menu('mvrcks_PROJmenu', query=True, exists=True): 
-            #     cmds.deleteUI('mvrcks_PROJmenu', menu=True) 
-            # mvrcks_SHOWmenu = cmds.menu('mvrcks_PROJmenu', parent=maya_main_window,tearOff=True, label=sgProject.upper())
+            del os.environ["XBMLANGPATH"]
+            os.environ["XBMLANGPATH"] = os.environ["SHARED_MAYA_DIR"] + "/icons/"
 
             if version == "2020":
                 self.logger.debug("[PIPELINE] Setting up Maya 2020.x environment variables")
-                os.environ["MAYA_MODULE_PATH"]= "/opt/walter/maya:/array/X/Library/MayaScripts/modules/Yeti:/array/X/Library/MayaScripts/modules/Yeti/bin:/array/X/Library/MayaScripts/modules/HoudiniEngine"
+                os.environ["MAYA_MODULE_PATH"]= "/opt/walter/maya:/array/X/Library/renderer/yeti/maya2020:/array/X/Library/renderer/yeti/maya2020/bin:/array/X/Library/MayaScripts/modules/HoudiniEngine"
+
+                # enable Maya Render Setup
+                del os.environ["MAYA_ENABLE_LEGACY_RENDER_LAYERS"]
+                os.environ["MAYA_RENDER_SETUP_DISPLAY_RS_NODES"] = "1"
+                os.environ["MAYA_RENDER_SETUP_GLOBAL_TEMPLATE_PATH"] = "/array/X/Library/3d/Maya_Templates/LookDev/renderSetup/RSTemplates"
+                os.environ["MAYA_RENDER_SETUP_GLOBAL_PRESETS_PATH"] = "/array/X/Library/3d/Maya_Templates/LookDev/renderSetup/Presets"
 
                 # Arnold
                 #del os.environ["MTOA_HOME"]
@@ -96,7 +91,22 @@ class BeforeAppLaunch(tank.Hook):
                 os.environ["MAYA_PACKAGE_PATH"] = os.environ["SHARED_MAYA_DIR"] + "/modules/MayaBonusTools-2017-2020"
 
                 # multiverse
-                tank.util.append_path_to_env_var("MAYA_MODULE_PATH", "/mav/stor/sw/lin/opt/multiverse/multiverse/Maya")
+                os.environ["MV_ROOT"] = "/mav/stor/sw/lin/opt/multiverse/USD"
+                os.environ[" USD_PLUG_IN_PATH "] = os.environ["MV_ROOT"] + "/lib/plugin/usd"
+                tank.util.append_path_to_env_var("PYTHON_PATH", os.environ["MV_ROOT"] + "/lib/python" )
+                tank.util.append_path_to_env_var("PATH", os.environ["MV_ROOT"] + "/bin")
+                tank.util.append_path_to_env_var("LD_LIBRARY_PATH", os.environ["MV_ROOT"] +  "/lib")
+                tank.util.append_path_to_env_var("MAYA_MODULE_PATH", os.environ["MV_ROOT"] + "/Maya")
+
+                # Used by Maya to Arnold and Arnold standalone
+                tank.util.append_path_to_env_var("ARNOLD_PLUGIN_PATH", os.environ["MV_ROOT"] + "/lib/procedurals/arnold")
+
+                # Used by Renderman for Maya
+                tank.util.append_path_to_env_var("RFM_PLUGINS_PATH", os.environ["MV_ROOT"] + "/lib/procedurals/renderman")
+
+                # NOTE: As of 3Delight NSI no searchpath is needed for procedurals
+
+                print ( "Multiverse Environment is set" )
 
                 #Red9Studios
                 os.environ["RED9STUDIOPACK_HOME"] = "/array/X/Library/MayaScripts/plugins/Red9StudioPack"
@@ -120,13 +130,20 @@ class BeforeAppLaunch(tank.Hook):
                 tank.util.append_path_to_env_var(os.environ["MAYA_SCRIPT_PATH"], os.environ["USD_ROOT"] + "/third_party/maya/lib/usd/usdMaya/resources")
                 tank.util.append_path_to_env_var(os.environ["MAYA_SCRIPT_PATH"], os.environ["USD_ROOT"] + "/third_party/maya/plugin/pxrUsdPreviewSurface/resources")
                 tank.util.append_path_to_env_var(os.environ["MAYA_SCRIPT_PATH"], os.environ["USD_ROOT"] + "/plugin/usd/usdAbc/resources")
+                
+                # Yeti
+                del os.environ["YETI_HOME"]
+                os.environ["YETI_HOME"] = "/array/X/Library/renderer/yeti/maya2020"
+                os.environ["DL_PROCEDURALS_PATH"] = os.environ["YETI_HOME"] + "/bin:" + os.environ["YETI_HOME"] + "/plug-ins"
+                tank.util.append_path_to_env_var(os.environ["MAYA_PLUG_IN_PATH"], os.environ["YETI_HOME"] + "/plug-ins")
+                tank.util.append_path_to_env_var("XBMLANGPATH", os.environ["YETI_HOME"] + "/icons")
 
                 # V-Ray
                 del os.environ["VRAY_DIR"]
                 os.environ["VRAY_DIR"] = os.environ["MAYA_RENDER_ENGINE"] + "/ChaosGroup/V-Ray/Maya2020"
-                os.environ["VRAY_PLUGINS_x86"] = os.environ["VRAY_DIR"] + "/maya_vray/vrayplugins:" + os.environ["YETI_HOME"] + "/bin"
+                os.environ["VRAY_PLUGINS_x86"] = os.environ["VRAY_DIR"] + "/maya_vray/vrayplugins:" + os.environ["YETI_HOME"] + "/bin:" + os.environ["MV_ROOT"] + "/lib/procedurals/vray"
                 os.environ["VRAY_FOR_MAYA2020_MAIN"] = os.environ["VRAY_DIR"] + "/maya_vray"
-                os.environ["VRAY_FOR_MAYA2020_PLUGINS"] = os.environ["VRAY_DIR"] + "/maya_vray/plug-ins:" + os.environ["VRAY_DIR"] + "/maya_vray/vrayplugins:" + os.environ["YETI_HOME"] + "/bin"
+                os.environ["VRAY_FOR_MAYA2020_PLUGINS"] = os.environ["VRAY_DIR"] + "/maya_vray/plug-ins:" + os.environ["VRAY_DIR"] + "/maya_vray/vrayplugins:" + os.environ["YETI_HOME"] + "/bin:" + os.environ["MV_ROOT"] + "/lib/procedurals/vray"
                 os.environ["VRAY_OSL_PATH_MAYA_MAYA2020"] = os.environ["VRAY_DIR"] + "/vray/opensl"
                 os.environ["MAYA_RENDER_DESC_PATH"] = os.environ["VRAY_DIR"] + "/maya_root/bin/rendererDesc"
                 os.environ["VRAY_TOOLS_MAYA2020"] = os.environ["VRAY_DIR"] + "/vray/bin"
@@ -135,41 +152,45 @@ class BeforeAppLaunch(tank.Hook):
                 tank.util.append_path_to_env_var("MAYA_PLUG_IN_PATH", os.environ["VRAY_DIR"] + "/maya_vray/plug-ins")
                 tank.util.append_path_to_env_var("LD_LIBRARY_PATH", os.environ["VRAY_DIR"] + "/maya_root/lib")
                 tank.util.append_path_to_env_var("MAYA_SCRIPT_PATH", os.environ["VRAY_DIR"] + "/maya_vray/scripts")
-
-                # Yeti
-                # os.environ["YETI_HOME"] = "/array/X/Library/MayaScripts/modules/Yeti"
-                # os.environ["VRAY_TOOLS_MAYA2020"] = os.environ["VRAY_DIR"] + "/vray/bin"
-                # os.environ["DL_PROCEDURALS_PATH"] = os.environ["YETI_HOME"] + "/bin:" + os.environ["YETI_HOME"] + "/plug-ins"
-                # tank.util.append_path_to_env_var(os.environ["MAYA_PLUG_IN_PATH"], os.environ["YETI_HOME"] + "/plug-ins")
-
-                # Zync
-                # os.environ["ZYNC_API_DIR"] = "/array/X/Library/scripts/zync-python"
-                # os.environ["ZYNC_PATH"] = "/array/X/Library/Software/zync"
-                # tank.util.append_path_to_env_var("PYTHONPATH",
-                # tank.util.append_path_to_env_var("PYTHONPATH",os.environ["ZYNC_PATH"] + "/zync-maya")
-                # tank.util.append_path_to_env_var("PYTHONPATH", "/array/X/Library/scripts/zync-python:/array/X/Library/scripts/zync-maya")
+                tank.util.append_path_to_env_var("XBMLANGPATH", os.environ["VRAY_DIR"] + "/maya_vray/icons/")
 
             if version == "2018":
                 self.logger.debug("[PIPELINE] Setting up Maya 2018.x environment variables")
+
+                # Yeti 3.6.4
+                #os.environ["MAYA_MODULE_PATH"]= "/opt/walter/maya:/array/X/Library/renderer/yeti/maya2018:/array/X/Library/renderer/yeti/maya2018/bin:/array/X/Library/MayaScripts/modules/HoudiniEngine"
                 os.environ["MAYA_MODULE_PATH"]= "/opt/walter/maya:/array/X/Library/MayaScripts/modules/Yeti:/array/X/Library/MayaScripts/modules/Yeti/bin:/array/X/Library/MayaScripts/modules/HoudiniEngine"
 
                 # disable Maya Render Setup
                 os.environ["MAYA_ENABLE_LEGACY_RENDER_LAYERS"] = "1"
 
                 # multiverse
-                tank.util.append_path_to_env_var("MAYA_MODULE_PATH", "/mav/stor/sw/lin/opt/multiverse/multiverse/Maya")
+                os.environ["MV_ROOT"] = "/mav/stor/sw/lin/opt/multiverse/multiverse"
+                os.environ[" USD_PLUG_IN_PATH "] = os.environ["MV_ROOT"] + "/lib/plugin/usd"
+                tank.util.append_path_to_env_var("PYTHON_PATH", os.environ["MV_ROOT"] + "/lib/python" )
+                tank.util.append_path_to_env_var("PATH", os.environ["MV_ROOT"] + "/bin")
+                tank.util.append_path_to_env_var("LD_LIBRARY_PATH", os.environ["MV_ROOT"] +  "/lib")
+                tank.util.append_path_to_env_var("MAYA_MODULE_PATH", os.environ["MV_ROOT"] + "/Maya")
+
+                # Used by Maya to Arnold and Arnold standalone
+                tank.util.append_path_to_env_var("ARNOLD_PLUGIN_PATH", os.environ["MV_ROOT"] + "/lib/procedurals/arnold")
+
+                # Used by Renderman for Maya
+                tank.util.append_path_to_env_var("RFM_PLUGINS_PATH", os.environ["MV_ROOT"] + "/lib/procedurals/renderman")
+
+                # NOTE: As of 3Delight NSI no searchpath is needed for procedurals
+
+                print ( "Multiverse Environment is set" )
 
                 # SpeedTree
                 os.environ["SPEEDTREEDIR"] = os.environ["SHARED_MAYA_DIR"] + "/modules/SpeedTree/Maya"
                 tank.util.append_path_to_env_var("MAYA_PLUG_IN_PATH", os.environ["SPEEDTREEDIR"])
 
                 # V-Ray
-                del os.environ["VRAY_DIR"]
-                #os.environ["VRAY_DIR"] = os.environ["MAYA_RENDER_ENGINE"] + "/ChaosGroup/V-Ray/Maya"
-                os.environ["VRAY_DIR"] = "/array/X/adrian/ChaosGroup/V-Ray/Maya"
+                os.environ["VRAY_DIR"] = os.environ["MAYA_RENDER_ENGINE"] + "/ChaosGroup/V-Ray/Maya2018"
                 os.environ["VRAY_PLUGINS_x86"] = os.environ["VRAY_DIR"] + "/maya_vray/vrayplugins:" + os.environ["YETI_HOME"] + "/bin"
                 os.environ["VRAY_FOR_MAYA2018_MAIN_x64"] = os.environ["VRAY_DIR"] + "/maya_vray"
-                os.environ["VRAY_FOR_MAYA2018_PLUGINS_x64"] = os.environ["VRAY_DIR"] + "/maya_vray/plug-ins:" + os.environ["VRAY_DIR"] + "/maya_vray/vrayplugins:" + os.environ["YETI_HOME"] + "/bin"
+                os.environ["VRAY_FOR_MAYA2018_PLUGINS_x64"] = os.environ["VRAY_DIR"] + "/maya_vray/plug-ins:" + os.environ["VRAY_DIR"] + "/maya_vray/vrayplugins:" + os.environ["YETI_HOME"] + "/bin:" + os.environ["MV_ROOT"] + "/lib/procedurals/vray"
                 os.environ["VRAY_OSL_PATH_MAYA_MAYA2018_x64"] = os.environ["VRAY_DIR"] + "/vray/opensl"
                 os.environ["MAYA_RENDER_DESC_PATH"] = os.environ["VRAY_DIR"] + "/maya_root/bin/rendererDesc"
                 os.environ["VRAY_TOOLS_MAYA2020"] = os.environ["VRAY_DIR"] + "/vray/bin"
@@ -178,12 +199,16 @@ class BeforeAppLaunch(tank.Hook):
                 tank.util.append_path_to_env_var("MAYA_PLUG_IN_PATH", os.environ["VRAY_DIR"] + "/maya_vray/plug-ins")
                 tank.util.append_path_to_env_var("LD_LIBRARY_PATH", os.environ["VRAY_DIR"] + "/maya_root/lib")
                 tank.util.append_path_to_env_var("MAYA_SCRIPT_PATH", os.environ["VRAY_DIR"] + "/maya_vray/scripts")
+                tank.util.append_path_to_env_var("XBMLANGPATH", os.environ["VRAY_DIR"] + "/maya_vray/icons/")
 
                 # Yeti
+                del os.environ["YETI_HOME"]
                 os.environ["YETI_HOME"] = "/array/X/Library/MayaScripts/modules/Yeti"
                 os.environ["VRAY_TOOLS_MAYA2018"] = os.environ["VRAY_DIR"] + "/vray/bin"
                 os.environ["DL_PROCEDURALS_PATH"] = os.environ["YETI_HOME"] + "/bin:" + os.environ["YETI_HOME"] + "/plug-ins"
                 tank.util.append_path_to_env_var(os.environ["MAYA_PLUG_IN_PATH"], os.environ["YETI_HOME"] + "/plug-ins")
+                tank.util.append_path_to_env_var("XBMLANGPATH", os.environ["YETI_HOME"] + "/icons")
+
 
                 # Arnold
                 #del os.environ["MTOA_HOME"]
